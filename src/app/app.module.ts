836fe287939dcfa0100f3bff552c3362fb3c0564
Niels <n.van.de.weem@smile.nl>
2020-02-27 15:38:40 +0100

    Add failing code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Injector, NgModule} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CompComponent} from './comp/comp.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatCardModule} from '@angular/material/card';
+import {MatRippleModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatRippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(injector: Injector) {
+    const elem = createCustomElement(CompComponent, {injector});
+    customElements.define('ce-comp', elem);
+  }
+}
---
+++ src/app/comp/comp.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp',
+  templateUrl: './comp.component.html',
+  styleUrls: ['./comp.component.scss']
+})
+export class CompComponent {
+}
